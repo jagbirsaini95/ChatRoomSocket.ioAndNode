3539f2e011d8c20bf16f2d5af38333985599b73b
jagbir singh <jagbirsaini95@gmail.com>
2023-06-18 10:38:37 +0530

    append error fixed

--- chatRoom-UI/js/chat.js
+++ chatRoom-UI/js/chat.js
@@ -1,6 +1,6 @@
 const socket = io('http://localhost:8000');
 
-const messageBody = document.getElementsByClassName('.box-body')
+const messageBody = document.querySelector('.box-body')
 const message = document.getElementById('message');
 const sendForm = document.getElementById('send-form');
 
@@ -11,12 +11,11 @@ const appendMessage = (message, position) => {
     messageBox.classList.add('border');
     messageBox.classList.add(position);
     messageBody.append(messageBox)
-
 }
 
 const userName = prompt('Please enter your name to join chat ');
 socket.emit('new-user-joined', user = { name: userName })
 
 socket.on('user-joined', (user) => {
-    appendMessage(`${user.name} joined the chat room`, 'right');
+    appendMessage(`${user.name} joined the chat room`, 'left');
 })
