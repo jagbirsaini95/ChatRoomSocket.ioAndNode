5f941c0f8ffd285c47c95dcb4333319f340ea372
jagbir singh <jagbirsaini95@gmail.com>
2023-06-26 16:32:26 +0530

    chat room leave done

--- chatRoom-APP/index.js
+++ chatRoom-APP/index.js
@@ -3,15 +3,27 @@ const io = require('socket.io')({ cors: '*' });
 const users = {};
 
 io.on('connection', (socket) => {
+    let userName;
     socket.on('new-user-joined', (user) => {
-        console.log("new user joined", user);
+        // console.log("new user joined", user);
         users[socket.id] = user;
+        userName= users[socket.id].name;
+        socket.emit('you-joined', user)
         socket.broadcast.emit('user-joined', user);
     })
     socket.on('send-message', (message) => {
-        console.log(message);
-        socket.broadcast.emit('recieve', user = { message: message, name: users[socket.id].name })
+        // console.log(userName,": ",message);
+        socket.broadcast.emit('recieve', user = { message: message, name: userName })
     })
+    socket.on('disconnected', (user)=>{
+        // console.log("user diconnected:",user);
+        socket.emit('you-disconnected', user = { message: `you left the room`, name: userName })
+        socket.broadcast.emit('user-disconnected', user = { message: `${userName} left the room`, name: userName })
+        socket.disconnect();
+    })
+})
+io.on('disconnect', (socket) => {
+    
 })
 
 const port = 8000;
--- chatRoom-UI/js/chat.js
+++ chatRoom-UI/js/chat.js
@@ -3,33 +3,93 @@ const socket = io('http://localhost:8000');
 const messageBody = document.querySelector('.box-body')
 const message = document.getElementById('message');
 const sendForm = document.getElementById('send-form');
+const closeBtn = document.getElementById('close-btn');
+const endChatBtn = document.getElementById('end-chat-btn');
+const chatroomContainer = document.getElementById('chatroom-container');
+const userNameContainer = document.getElementById('user-name');
+const chatroomIcon = document.getElementById('chatroom-icon');
 
 const appendMessage = (message, position) => {
     console.log(message);
     const messageBox = document.createElement('div');
-    messageBox.innerText = message;
-    messageBox.classList.add('border');
+    messageBox.innerHTML = message;
+    messageBox.classList.add('border-0');
     messageBox.classList.add(position);
     messageBody.append(messageBox)
 }
 
+const scrollToBottom=(el)=>{
+    let height = el.scrollHeight;
+    el.scrollTop=height;
+}
+
+const closeChatbot=()=>{
+    console.log("chatbot closed");
+    chatroomContainer.classList.add('display-none')
+    chatroomIcon.classList.remove('display-none')
+}
+
+const openChatbot=()=>{
+    console.log("chatbot opened");
+    chatroomIcon.classList.add('display-none')
+    chatroomContainer.classList.remove('display-none')
+    message.focus();
+}
+
 sendForm.addEventListener('submit', (e) => {
-    console.log(e);
     e.preventDefault();
+    console.log(e.target[0]);
     const message = e.target[0].value;
-    appendMessage(`you: ${message}`, 'right')
+    appendMessage(`you: <b class="capitalize">${message}</b>`, 'right')
     socket.emit('send-message', message)
-    message.value = '';
+    e.target[0].value = '';
+    scrollToBottom(messageBody);
+    message.focus();
 })
 
-const userName = prompt('Please enter your name to join chat ');
+closeBtn.addEventListener('click', () => {
+    closeChatbot();
+})
+
+chatroomIcon.addEventListener('click', () => {
+    openChatbot();
+})
+
+endChatBtn.addEventListener('click', () => {
+    socket.emit('disconnected',userName)
+    endChatBtn.style.display="none";
+    closeChatbot();
+})
+let userName;
+do{
+     userName = prompt('Please enter your name to join chat ');
+}while(!userName || userName.length<4)
+
 socket.emit('new-user-joined', user = { name: userName })
 
 socket.on('user-joined', (user) => {
-    appendMessage(`${user.name} joined the chat room`, 'center');
+    appendMessage(`<b class="upper-case">${user.name}</b> joined the chat room`, 'center');
+})
+
+socket.on('you-joined', (user) => {
+    openChatbot();
+    appendMessage(`you joined the chat room`, 'center');
+    userNameContainer.innerHTML =`Logged in as:<b class="upper-case"> ${user.name} </b>`;
+    userNameContainer.classList.add('border');
+    userNameContainer.style.textAlign="right"
 })
 
 socket.on('recieve', (user) => {
     console.log(user);
-    appendMessage(`${user.message}: ${user.name}`, 'left');
+    appendMessage(`<b class="capitalize">${user.message}</b>: ${user.name}`, 'left');
+})
+
+socket.on('you-disconnected', (user) => {
+    console.log(user);
+    appendMessage(`${user.message}`, 'center');
+})
+
+socket.on('user-disconnected', (user) => {
+    console.log(user);
+    appendMessage(`${user.message}`, 'center');
 })
