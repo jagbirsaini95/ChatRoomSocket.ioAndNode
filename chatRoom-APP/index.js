3f31e517ecbda7cd5de898a418aa5fb481078257
jagbir singh <jagbirsaini95@gmail.com>
2025-02-19 16:08:14 +0530

    identation and lint

--- chatRoom-APP/index.js
+++ chatRoom-APP/index.js
@@ -7,23 +7,23 @@ io.on('connection', (socket) => {
     socket.on('new-user-joined', (user) => {
         // console.log("new user joined", user);
         users[socket.id] = user;
-        userName= users[socket.id].name;
-        socket.emit('you-joined', user)
+        userName = users[socket.id].name;
+        socket.emit('you-joined', user);
         socket.broadcast.emit('user-joined', user);
-    })
+    });
     socket.on('send-message', (message) => {
         // console.log(userName,": ",message);
-        socket.broadcast.emit('recieve', user = { message: message, name: userName })
-    })
-    socket.on('disconnected', (user)=>{
+        socket.broadcast.emit('recieve', user = { message: message, name: userName });
+    });
+    socket.on('disconnected', (user) => {
         // console.log("user diconnected:",user);
-        socket.emit('you-disconnected', user = { message: `you left the room`, name: userName })
-        socket.broadcast.emit('user-disconnected', user = { message: `${userName} left the room`, name: userName })
+        socket.emit('you-disconnected', user = { message: `you left the room`, name: userName });
+        socket.broadcast.emit('user-disconnected', user = { message: `${userName} left the room`, name: userName });
         socket.disconnect();
     })
 })
 io.on('disconnect', (socket) => {
-    
+
 })
 
 const port = 8000;
